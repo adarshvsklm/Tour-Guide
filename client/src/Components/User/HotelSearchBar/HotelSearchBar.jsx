f5428f82fe47716fbbc9cde6f4d292944ac4c83f
adarshvsklm <adarshvsklm@gmail.com>
2022-08-24 12:34:32 +0530

    datePicker

--- client/src/Components/User/HotelSearchBar/HotelSearchBar.jsx
+++ client/src/Components/User/HotelSearchBar/HotelSearchBar.jsx
@@ -1,140 +1,108 @@
-import React from 'react'
-import './SearchBarStyle.css'
+import React from 'react';
+import './SearchBarStyle.css';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import { makeStyles } from '@mui/styles';
+import { createTheme } from '@mui/system';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBed,
+  faCalendarDays,
+  faCar,
+  faPerson,
+  faPlane,
+  faTaxi,
+} from '@fortawesome/free-solid-svg-icons';
 
+const theme = createTheme();
+const useStyles = makeStyles(() => ({
+  root: {
+    minHeight: '25vh',
+    [theme.breakpoints.up('md')]: {
+      minHeight: '30vh',
+    },
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: '#265299',
+    alignItems: 'center',
+    background: 'black',
+    position: 'sticky',
+    top: 0,
+  },
+  searchBar: {
+    color: '#5AFF3D',
+  },
+}));
 function HotelSearchBar() {
+  const classes = useStyles();
   return (
-    <div className='headerSearch'  >
-        <div
-        style={{
-          height: '200px',
-          backgroundColor: '#265299',
-          position: 'sticky',
-          top: 0,
-        }}
-      >
-        <div className='text float-bottom'>
-        <span >jhkjhknnnnnnnnnnnnnnnnnnnnnnnnnnjhkj</span>
+    <div>
+      <div className={classes.root}>
+        {/* <span className={classes.searchBar}>search bar</span> */}
+        <div className='headerSearch'>
+          <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faBed} className='headerIcon' />
+            <input
+              type='text'
+              placeholder='Where are you going ?'
+              className='headerSearchInput'
+            />
+          </div>
+          <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
+             <span className='headerSearchText'>date to date</span>
+          </div>
+          <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faPerson} className='headerIcon' />
+            <span className='headerSearchText'>2 adults 2 children 1 room</span>
+
+          </div>
         </div>
-       <br />
       </div>
-      djncjsssssssssssssss
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
-      <br />
-      djncj
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
+      jhdgjhgvj <br />
     </div>
-  )
+  );
 }
 
-export default HotelSearchBar
+export default HotelSearchBar;
